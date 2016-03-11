2dba6257eea8c04bda098b8586253aae5f34d5b9
Parker Donat <parkerdonat@gmail.com>
2016-03-11 12:11:57 -0700

    Finished Gifter model

--- GiftExchangeRandomizer/GiftExchangeRandomizer/AddGifterViewController.swift
+++ GiftExchangeRandomizer/GiftExchangeRandomizer/AddGifterViewController.swift
@@ -11,19 +11,20 @@ import UIKit
 class AddGifterViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
+    
+    var gifter: Gifter?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func addGifterButtonTapped(sender: AnyObject) {
         
+    
     }
 
     @IBAction func cancelButtonTapped(sender: AnyObject) {
--- GiftExchangeRandomizer/GiftExchangeRandomizer/Gifter.swift
+++ GiftExchangeRandomizer/GiftExchangeRandomizer/Gifter.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+
+class Gifter: Equatable {
+    
+    private let kGifterName = "name"
+    
+    var name: String
+
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    init?(dictionary: [String: AnyObject]) {
+        guard let name = dictionary[kGifterName] as? String else {
+            self.name = ""
+            
+            return nil
+        }
+        
+        self.name = name
+    }
+    
+    func dictionaryCopy() -> [String: AnyObject] {
+        let dictionary = [
+            kGifterName : self.name
+        ]
+        return dictionary
+    }
+}
+
+func == (lhs: Gifter, rhs: Gifter) -> Bool {
+    return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+}
+
